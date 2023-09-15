c4394d54496c0a05c32abc3e71abaf1a3ee9d7b3
IconicDexter <kennymwendwa67@gmail.com>
2023-09-15 11:25:17 +0300

    npm i react-syntax-highlighter

--- components/ConvertedText.tsx
+++ components/ConvertedText.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function ConvertedText() {
   return (
     <div className="container mx-auto p-2">
       <div className="text-xl flex justify-center">ConvertedText</div>
-      <ReactMarkdown># **Hello**, *world*!</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>e</ReactMarkdown>
     </div>
   );
 }
