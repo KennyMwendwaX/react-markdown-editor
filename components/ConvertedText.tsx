1f53fb17acd6bcb0379ece3cad856ec4924f2bc9
IconicDexter <kennymwendwa67@gmail.com>
2023-09-15 12:44:09 +0300

    npm uninstall react-syntax-highlighter react-markdown remark-gfm

--- components/ConvertedText.tsx
+++ components/ConvertedText.tsx
@@ -4,10 +4,26 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export default function ConvertedText() {
+  const markdown = `
+  # **Hello**
+  paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+  
+  * Lists
+  * [ ] todo
+  * [x] done
+  
+  A table:
+  
+  | a | b |
+  | - | - |
+
+   `;
   return (
     <div className="container mx-auto p-2">
       <div className="text-xl flex justify-center">ConvertedText</div>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>e</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </div>
   );
 }
