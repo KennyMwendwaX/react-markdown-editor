79c0814ab51b586399caf92f7a6bcb22b4276dc7
IconicDexter <kennymwendwa67@gmail.com>
2023-09-15 13:58:54 +0300

    Integrated highlight.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 "use client";
 import MarkdownIt from "markdown-it";
 import MdEditor from "react-markdown-editor-lite";
-// import style manually
 import "react-markdown-editor-lite/lib/index.css";
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
 
 export default function Home() {
+  // Then register the languages you need
+  hljs.registerLanguage("javascript", javascript);
   // Initialize a markdown parser
-  const mdParser = new MarkdownIt(/* Markdown-it options */);
+  const mdParser = new MarkdownIt(
+    /* Markdown-it options */
+    {
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(str, { language: lang }).value;
+          } catch (__) {}
+        }
+
+        return ""; // use external default escaping
+      },
+    }
+  );
 
   function handleEditorChange({ html, text }: { html: string; text: string }) {
     console.log("handleEditorChange", html, text);
