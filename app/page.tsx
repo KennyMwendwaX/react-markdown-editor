80558bd1f5360960a495165c4b27a8e71551a6dc
IconicDexter <kennymwendwa67@gmail.com>
2023-09-15 13:09:42 +0300

    Integrated react-markdown-editor-lite

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+"use client";
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
 export default function Home() {
+  // Initialize a markdown parser
+  const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+  function handleEditorChange({ html, text }: { html: string; text: string }) {
+    console.log("handleEditorChange", html, text);
+  }
   return (
     <div className="bg-gray-50 text-black">
       <div className="text-3xl flex justify-center p-3">
         React Markdown Editor
       </div>
-      <div className=""></div>
+      <div className="">
+        <MdEditor
+          style={{ height: "500px" }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={handleEditorChange}
+        />
+      </div>
     </div>
   );
 }
