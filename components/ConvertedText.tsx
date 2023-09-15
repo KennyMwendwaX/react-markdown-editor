2ed0cd4bea000b73f038e6b69607acc262490c01
IconicDexter <kennymwendwa67@gmail.com>
2023-09-15 11:10:35 +0300

    npm i remark-gfm

--- components/ConvertedText.tsx
+++ components/ConvertedText.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import ReactMarkdown from "react-markdown";
+
 export default function ConvertedText() {
   return (
     <div className="container mx-auto p-2">
       <div className="text-xl flex justify-center">ConvertedText</div>
-      <div>Hi yall</div>
+      <ReactMarkdown># **Hello**, *world*!</ReactMarkdown>
     </div>
   );
 }
