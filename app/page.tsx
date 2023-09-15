1c1c3126475c1a67ff2eb051ebac4165158f4e0b
IconicDexter <kennymwendwa67@gmail.com>
2023-09-15 12:58:33 +0300

    npm i markdown-it react-markdown-editor-lite

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import ConvertedText from "@/components/ConvertedText";
-import Editor from "@/components/Editor";
-
 export default function Home() {
   return (
     <div className="bg-gray-50 text-black">
       <div className="text-3xl flex justify-center p-3">
         React Markdown Editor
       </div>
-      <div className="grid grid-cols-2 h-screen">
-        <Editor />
-        <ConvertedText />
-      </div>
+      <div className=""></div>
     </div>
   );
 }
