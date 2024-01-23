dcb7ebe6add737f32b8b77d2cd6b6d24b8a5c446
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-23 08:31:51 +0300

    Added dark mode to  MarkdownPreview

--- app/example2/page.tsx
+++ app/example2/page.tsx
@@ -12,26 +12,16 @@ export default function RichText() {
     // Reset the editor to the saved markdown
     editor.setMarkdown("# Hello World");
   };
-  const source = `
-## MarkdownPreview
 
-**Hello world!!!** <IFRAME SRC=\"javascript:javascript:alert(window.origin);\"></IFRAME>
-
-a
-
-alert("Hi yall");
-
-
-<!-- test --> 123
-
-<!-- test --> 456 <!-- test -->
-`;
+  // Added a plugin to sanitize the markdown
   const rehypePlugins = [rehypeSanitize];
 
   return (
     <div className="bg-white min-h-screen pb-4">
-      <div className="pt-16 mx-48">
-        <button className="text-black" onClick={resetMarkdown}>
+      <div className="pt-16 mx-48 space-y-1">
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
+          onClick={resetMarkdown}>
           Reset Markdown
         </button>
         <Editable
@@ -45,7 +35,14 @@ alert("Hi yall");
         <div className="text-3xl text-black font-semibold">
           Markdown Preview
         </div>
-        <MarkdownPreview source={source} rehypePlugins={rehypePlugins} />
+        <MarkdownPreview
+          className="border border-black rounded-xl"
+          source={markdown}
+          rehypePlugins={rehypePlugins}
+          wrapperElement={{
+            "data-color-mode": "dark",
+          }}
+        />
       </div>
     </div>
   );
